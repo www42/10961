6347042b4c983035b04b4e3b3383af29c13e8e83
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2019-01-28 16:53:44 +0100

    Lab1B, Lab2 uploaded

---
+++ 10961C Lab1B.ps1
@@ -0,0 +1,60 @@
+﻿# 10961C Lab 1B Finding and running basic commands
+# ------------------------------------------------
+
+# Exercise 1: Finding commands
+# ----------------------------
+
+Get-Command *resolve*
+Resolve-DnsName google.com
+
+Get-Command *adapter*
+Get-NetAdapter
+Get-Command -Noun NetAdapter
+Get-Help Set-NetAdapter
+Get-Help Set-NetAdapter -Parameter MacAddress
+
+Get-Command *sched*
+Get-ScheduledTask
+
+Get-Command *block*
+Get-Help Block-SmbShareAccess
+
+Get-Command *branch*
+# nothing
+
+Get-Command *cache*
+Get-Command -Verb clear
+Clear-DnsClientCache
+
+Get-Command *firewall*
+Get-NetFirewallRule
+
+Get-Command *address*
+Get-NetIPAddress
+
+Get-Command -Verb suspend
+
+Get-Command -Noun content
+
+
+# Exercise 2: Runnning commands
+# -----------------------------
+
+Get-NetFirewallRule -Enabled True
+Get-NetIPAddress -AddressFamily IPv4
+Set-Service -Name BITS -StartupType Automatic
+Test-NetConnection LON-DC1 -InformationLevel Quiet
+Get-EventLog -LogName Security -Newest 10
+
+
+# Exercise 3: Using about files
+# -----------------------------
+
+Get-Help *comparison*
+Get-Help about_comparison_operators -ShowWindow
+
+$env:COMPUTERNAME
+Get-Help about_environment_variables
+
+Get-Help *signing*
+Get-Help about_signing
---
+++ 10961C Lab2.ps1
@@ -0,0 +1,69 @@
+﻿# Exercise 1: Creating and managing Active Directory objects
+# ----------------------------------------------------------
+
+# -------
+# LON-CL1
+# -------
+
+# Create a new organizational unit (OU) for a branch office
+New-ADOrganizationalUnit -Name London
+
+# Create group for branch office administrators
+New-ADGroup "London Admins" -GroupScope Global
+
+# Create a user and computer account for the branch office
+New-ADUser -Name Ty -DisplayName "Ty Carlson"
+Add-ADGroupMember “London Admins” -Members Ty
+New-ADComputer LON-CL2
+
+# Move the group, user, and computer accounts to the branch office OU
+Move-ADObject -Identity "CN=London Admins,CN=Users,DC=Adatum,DC=com" -TargetPath "OU=London,DC=Adatum,DC=com"
+Move-ADObject -Identity "CN=Ty,CN=Users,DC=Adatum,DC=com" -TargetPath "OU=London,DC=Adatum,DC=com"
+Move-ADObject -Identity "CN=LON-CL2,CN=Computers,DC=Adatum,DC=com" -TargetPath "OU=London,DC=Adatum,DC=com"
+
+
+# Exercise 2: Configuring network settings on Windows Server
+# ----------------------------------------------------------
+
+# --------
+# LON-SVR1
+# --------
+
+# Test the network connection and view the configuration
+Test-Connection LON-DC1
+Get-NetIPConfiguration
+
+# Change the server IP address
+New-NetIPAddress -InterfaceAlias Ethernet -IPAddress 172.16.0.15 -PrefixLength 16
+Remove-NetIPAddress -InterfaceAlias Ethernet -IPAddress 172.16.0.11
+
+# Change the DNS settings and default gateway for the server
+Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddress 172.16.0.12
+Remove-NetRoute -InterfaceAlias Ethernet -DestinationPrefix 0.0.0.0/0 -Confirm:$false
+New-NetRoute -InterfaceAlias Ethernet -DestinationPrefix 0.0.0.0/0 -NextHop 172.16.0.2
+
+# Verify and test the changes
+Get-NetIPConfiguration
+
+
+# Exercise 3: Creating a website
+# ------------------------------
+
+# --------
+# LON-SVR1
+# --------
+
+# Install IIS on the server 
+Install-WindowsFeature Web-Server
+
+# Create a folder on the server for the website
+New-Item C:\inetpub\wwwroot\London -Type directory
+
+# Create a new application pool for the website
+New-WebAppPool LondonAppPool
+
+# Create the IIS website
+New-WebSite London -PhysicalPath C:\inetpub\wwwroot\london -IPaddress 172.16.0.15 -ApplicationPool LondonAppPool
+
+# On the taskbar, click the Internet Explorer icon. 
+# In the Address bar, type 172.16.0.15, and then press Enter.
