6e6aef0d13239c351f3c8a07f2d2869e7c8a48a1
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2019-01-30 13:43:35 +0100

    Lab 7 added

---
+++ 10961C Lab7.ps1
@@ -0,0 +1,31 @@
+﻿# 10961C Lab 7 Working with variables, arrays, and hash tables
+
+#region Working with variables
+
+$logPath = "C:\Logs\"
+$logPath.GetType()
+$logPath | Get-Member
+$logFile = "log.txt"
+$logPath += $logFile
+$logPath
+$logPath.Replace("C:","D:")
+$today = Get-Date
+$today.GetType()
+$today | Get-Member
+$logFile = [string]$today.Year + "-" + $today.Month + "-" + $today.Day + "-" + $today.Hour + "-" + $today.Minute + ".txt"
+$cutOffDate = $today.AddDays(-30)
+
+Get-ADUser -Properties LastLogonDate -Filter {LastLogonDate -gt $cutOffDate}
+
+#endregion
+
+#region Arrays
+
+$mktgUsers = Get-ADUser -Filter {Department -eq "Marketing"} -Properties Department
+$mktgUsers.Count
+$mktgUsers | Set-ADUser -Department "Business Development"
+
+Get-ADUser -Filter {Department -eq "Marketing"}
+Get-ADUser -Filter {Department -eq "Business Development"}
+
+#endregion
