5e390c9f4eff4299f46dbde9ae28df8ab75bd6d4
www42 <thomas.jaekel@gfn.de>
2020-08-26 08:23:51 +0200

    Lab 4 added

---
+++ 10961C Lab4.ps1
@@ -0,0 +1,22 @@
+﻿# Funktioniert der Befehl?
+# ------------------------
+
+# 2.
+Get-ADComputer -Filter * | Get-Service -Name *
+# --> Nein 
+
+# 3.
+Get-ADComputer -Filter * | Select-Object @{n='ComputerName';e={$_.Name}} | Get-Service -Name *
+# --> Ja (ByPropertyName)
+
+# 4.
+Get-ADComputer –Filter * | Select @{n='ComputerName';e={$_.Name}} | Get-WmiObject –Class Win32_BIOS
+# --> Nein
+
+# 5.
+Get-Service –ComputerName (Get-ADComputer –Filter *)
+# --> Nein
+
+# 6.
+Get-EventLog –LogName Security –ComputerName (Get-ADComputer LON-DC1 | Select –ExpandProperty Name)
+# --> Yes
