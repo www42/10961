c06d7675cedb0ae084131eca04c357f3056fafcf
www42 <thomas.jaekel@gfn.training>
2021-10-06 08:50:35 +0200

    Update Lab6.ps and Lab7.ps

--- 10961C Lab6.ps1
+++ 10961C Lab6.ps1
@@ -1,47 +1,74 @@
-﻿# Exercise 1: Querying information by using WMI
-# ---------------------------------------------
-# -------
-# LON-CL1
-# -------
+﻿# 10961 C Module 06
+# ==================
 
-# Query IP addresses
+# Exercise 1: Querying information by using WMI
+# ---------------------------------------------
+# 1. Sign in to LON-CL1
+# 2. Open PowerShell as Administrator
+# 3. Find Repository that Lists IP
 Get-WmiObject –namespace root\cimv2 –list | Where Name –like '*configuration*' | Sort Name
+
+# 4. Retrieve All Instances of Class Showing DHCP IP
 Get-WmiObject –Class Win32_NetworkAdapterConfiguration | Where DHCPEnabled –eq $False | Select IPAddress
 
-# Query operating-system version information
+# 5. Find Repository that List OS Information
 Get-WmiObject –namespace root\cimv2 –list | Where Name –like '*operating*' | Sort Name
+
+# 6. List Properties for Win32_OperatingSystem
 Get-WmiObject –Class Win32_OperatingSystem | Get-Member
+
+# 7. Display OS Version, Service Pack Version and Build
 Get-WmiObject –Class Win32_OperatingSystem | Select Version,ServicePackMajorVersion,BuildNumber
 
-# Query computer-system hardware information
+# 8. Find Repository Containing Computer-System Info
 Get-WmiObject –namespace root\cimv2 –list | Where Name –like '*system*' | Sort Name
+
+# 9. List Properties and Property Values for Class
 Get-WmiObject –class Win32_ComputerSystem | Format-List –Property *
+
+# 10. Show Computers Manufacturer, Model and RAM
 Get-WmiObject –class Win32_ComputerSystem | Select Manufacturer,Model,@{n='RAM';e={$PSItem.TotalPhysicalMemory}}
 
-# Query service information 
+# 11. Locate Repository Class for Service Information
 Get-WmiObject –namespace root\cimv2 –list | Where Name –like '*service*' | Sort Name
+
+# 12. List Properties and Property Values for Class
 Get-WmiObject –Class Win32_Service | FL *
+
+# 13. List Service Name, Status, and Sign-in Name
 Get-WmiObject –Class Win32_Service –Filter "Name LIKE 'S%'" | Select Name,State,StartName
+
+
 
 
 # Exercise 2: Querying information by using CIM
 # ---------------------------------------------
 
-# Query user accounts
+# 1. Locate Repository Class for User Accounts
 Get-WmiObject –namespace root\cimv2 –list | Where Name –like '*user*' | Sort Name 
+
+# 2. List Win32_UserAccount Properties
 Get-CimInstance –Class Win32_UserAccount | Get-Member
+
+# 3. List User Accounts
 Get-CimInstance –Class Win32_UserAccount | Format-Table –Property Caption,Domain,SID,FullName,Name
 
-# Query BIOS information
+# 4. Find Repository Class for BIOS Information
 Get-WmiObject –namespace root\cimv2 –list | Where Name –like '*bios*' | Sort Name
+
+# 5. List BIOS Information
 Get-CimInstance –Class Win32_BIOS
 
-# Query network adapter configuration information
+# 6. Display Win32_NetworkAdapterConfiguration Class
 Get-CimInstance –Classname Win32_NetworkAdapterConfiguration
+
+# 7. Display All Instances of Previous Class on LON-DC1
 Get-CimInstance –Classname Win32_NetworkAdapterConfiguration –ComputerName LON-DC1
 
-# Query user group information
+# 8. Find Class that Lists User Groups
 Get-WmiObject –namespace root\cimv2 –list | Where Name –like '*group*' | Sort Name
+
+# 9. List Groups that Exist on LON-DC1
 Get-CimInstance –ClassName Win32_Group –ComputerName LON-DC1
 
 
@@ -50,10 +77,14 @@ Get-CimInstance –ClassName Win32_Group –ComputerName LON-DC1
 # Exercise 3: Invoking methods
 # ----------------------------
 
-# Invoke a CIM method
+# 1. Restart LON-DC1 by using Win32_OperatingSystem
 Invoke-CimMethod –ClassName Win32_OperatingSystem –ComputerName LON-DC1 –MethodName Reboot
 
-# Invoke a WMI method
-Get-Service -Name WinRM | % StartType
+# 2. Switch to LON-DC1
+# 3. Switch to LON-CL1
+# 4. Open Computer Management
+# 5. View Windows Remote Management Startup Type
+# 6. Change WS-Management Startup Type
 Get-WmiObject –Class Win32_Service –Filter "Name='WinRM'" | Invoke-WmiMethod –Name ChangeStartMode –Argument 'Automatic'
-Get-Service -Name WinRM | % StartType
+
+# 7. Confirm Startup Type Changed
--- 10961C Lab7.ps1
+++ 10961C Lab7.ps1
@@ -1,54 +1,123 @@
-﻿# 10961C Lab 7 Working with variables, arrays, and hash tables
+﻿# 10961 C Module 07
+# ==================
 
-#region Working with variables
-
+# Exercise 1: Working with Variable Types
+# ----------------------------------------
+# 1. Sign in to LON-CL1
+# 2. Open PowerShell
+# 3. Create Variable for C:\logs
 $logPath = "C:\Logs\"
+
+# 4. Identify Type of Variable and Available Properties
 $logPath.GetType()
+
+# 5. Get $logPath Members
 $logPath | Get-Member
+
+# 6. Create Variable for log.txt
 $logFile = "log.txt"
+
+# 7. Update $logPath to Include $logFile
 $logPath += $logFile
-$logPath
-$logPath.Replace("C:", "D:")
-$logPath = $logPath.Replace("C:", "D:")
+
+# 8. Display Contents of $logPath
 $logPath
 
+# 9. Configure $logPath.Replace Variable
+$logPath.Replace("C:", "D:")
+
+# 10. Update $logPath to Use Drive D Instead of C
+$logPath = $logPath.Replace("C:", "D:")
+
+# 11. Display Contents of $logPath
+$logPath
+
+# 12. Create Variable for Today's Date
 $today = Get-Date
+
+# 13. Identify Type of Variable and Available Properties
 $today.GetType()
+
+# 14. Get $today Members
 $today | Get-Member
+
+# 15. Create String in Format Year-Month-Day-Hour-Minute
 $logFile = [string]$today.Year + "-" + $today.Month + "-" + $today.Day + "-" + $today.Hour + "-" + $today.Minute + ".txt"
+
+# 16. Create $cutOffDate Variable 30 Days Before Today
 $cutOffDate = $today.AddDays(-30)
+
+# 17. Get Accounts That have Signed in since $cutOffDay
 Get-ADUser -Properties LastLogonDate -Filter {LastLogonDate -gt $cutOffDate}
 
-#endregion
 
-#region Using Arrays
 
+
+# Exercise 2: Using Arrays
+# ----------------------------------------
+# 1. Add All AD DS Marketing Users into Variable
 $mktgUsers = Get-ADUser -Filter {Department -eq "Marketing"} -Properties Department
-$mktgUsers.Count                                            # 52
-$mktgUsers[0]                                               # Lizzie Terrell
-$mktgUsers | Set-ADUser -Department "Business Development"
-$mktgUsers | Format-Table Name,Department
-Get-ADUser -Filter {Department -eq "Marketing"}             # Niemand mehr
-Get-ADUser -Filter {Department -eq "Business Development"}  # die 52
 
+# 2. Identify Number of Marketing Users
+$mktgUsers.Count
+
+# 3. Display First User and Confirm Properties
+$mktgUsers[0]
+
+# 4. Update Department to Business Development
+$mktgUsers | Set-ADUser -Department "Business Development"
+
+# 5. Check if $mktgUsers Variable Updated
+$mktgUsers | Format-Table Name,Department
+
+# 6. Confirm No Users are in Marketing
+Get-ADUser -Filter {Department -eq "Marketing"}
+
+# 7. Confirm 52 Users are in Business Development
+Get-ADUser -Filter {Department -eq "Business Development"}
+
+# 8. Create Arraylist Named $computers
 [System.Collections.ArrayList]$computers = "LON-SRV1", "LON-SRV2", "LON-DC1"
+
+# 9. Confirm $computers Doesn't Have a Fixed Size
 $computers.IsFixedSize
+
+# 10. Add LON-DC2 to $computers
 $computers.Add("LON-DC2")
+
+# 11. Remove LON-SRV2 from $computers
 $computers.Remove("LON-SRV2")
+
+# 12. Display Computers in $computers
 $computers
 
-#endregion
 
-#region Using Hash Tables
+
+
+
+# Exercise 3: Using Hash Tables
+# -----------------------------
+# 1. Create Hash Table Name $mailList
 $mailList = @{"Frank" = "Frank@fabriakm.com"; `
               "Libby" = "LHayward@contso.com"; `
               "Matej" = "MSTaojanov@tailspintoys.com"}
 
-$mailList
-$mailList.Libby
-$mailList.Libby = "Libby.Hayward@contoso.com"
-$mailList.Add("Stela", "Stela.Sahiti")
-$mailList.Remove("Frank")
+# 2. Display $mailList Contents
 $mailList
 
-#endregion
+# 3. Display Email for Libby
+$mailList.Libby
+
+# 4. Update Email for Libby
+$mailList.Libby = "Libby.Hayward@contoso.com"
+
+# 5. Add New Email for Stela
+$mailList.Add("Stela", "Stela.Sahiti")
+
+# 6. Remove Frank From $mailList
+$mailList.Remove("Frank")
+
+# 7. Confirm Frank was Removed
+$mailList
+
+# 8. Close PowerShell
