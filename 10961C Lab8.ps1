52ba6bd8d02aa91229f5aa1f3da5f640eca32452
www42 <thomas.jaekel@gfn.training>
2021-10-06 10:57:15 +0200

    Update Lab8.ps1

--- 10961C Lab8.ps1
+++ 10961C Lab8.ps1
@@ -1,27 +1,57 @@
-﻿# 10961C Lab 8 Basic Scripting
+﻿# 10961 C Module 08
+# ==================
 
-#region Signing a Script
-
+# Exercise 1: Signing a Script
+# ----------------------------
+#  1. Sign in to LON-CL1
+#  2. Open MMC
 & mmc
-# Request new cert
 
-$cert = dir Cert:\CurrentUser\My -CodeSigningCert
+#  3. Begin Adding Snap-In
+#  4. Add Certificates Snap-In
+#  5. Select Account to Manage Certificates for
+#  6. Close Add or Remove Snap-Ins
+#  7. Start Certificate Enrollment Wizard
+#  8. Advance Wizard
+#  9. Select Enrollment Policy
+# 10. Select Certificate to Enroll In
+# 11. Click Finish
+# 12. Confirm Code Signing Certificate was Added
+# 13. Close MMC Console
+# 14. Open PowerShell
+# 15. Place Code Signing Certificate in CurrentUser\My
+Get-ChildItem Cert:\CurrentUser\My\ -CodeSigningCert
 
+# 16. Set $cert Value
+$cert = Get-ChildItem Cert:\CurrentUser\My\ -CodeSigningCert
+
+# 17. Set Work Location
 Set-Location E:\Mod08\Labfiles
+
+# 18. Rename HelloWorld.txt to HelloWorld.ps1
 Rename-Item .\HelloWorld.txt .\HelloWorld.ps1
+
+# 19. Apply Digital Certificate to HelloWorld.ps1
 Set-AuthenticodeSignature -FilePath .\HelloWorld.ps1 -Certificate $cert
-psedit E:\Mod08\Labfiles\HelloWorld.ps1
 
+# 20. Change Execution Policy to All Signed
 Set-ExecutionPolicy AllSigned -Force
-Get-ExecutionPolicy
 
+# 21. Run HelloWorld.ps1
 .\HelloWorld.ps1
 
+# 22. Change Execution Policy to Unrestricted
 Set-ExecutionPolicy Unrestricted -Force
 
-#endregion
+# 23. Close PowerShell
 
-#region Processing an Array with a ForeEach Loop
+
+
+
+
+
+# Exercise 2: Processing an Array with a ForeEach Loop
+# ----------------------------------------------------
 
 New-ADGroup -Name IPPhoneTest -GroupScope Universal -GroupCategory Security
 Move-ADObject "CN=IPPhoneTest,CN=Users,DC=Adatum,DC=com" -TargetPath "OU=IT,DC=Adatum,DC=com"
@@ -39,9 +69,10 @@ Get-ADUser -Identity Abbi -Properties ipPhone | ft Name, ipPhone
 Get-ADUser -Identity Beth -Properties ipPhone | ft Name, ipPhone
 
 
-#endregion
 
-#region Processing Items by Using If Statements
+
+# Exercise 3: Processing Items by Using If Statements
+# ---------------------------------------------------
 
 Set-Location E:\Mod08\Labfiles
 New-Item services.txt -ItemType File
@@ -61,9 +92,10 @@ foreach ($s in $Services) {
     }
 }
 
-#endregion
 
-#region Creating a Random Password
+
+# Exercise 4: Creating a Random Password
+# --------------------------------------
 
 $passwordLength = 8
 $password = $null
@@ -74,9 +106,10 @@ for ($i = 0; $i -le $passwordLength; $i++) {
 }
 Write-Output "The password is: $password"
 
-#endregion
 
-#region Creating Users Based on a CSV File
+
+# Exercise 5: Creating Users Based on a CSV File
+# ----------------------------------------------
 
 $users = Import-Csv -Path users.csv
 ForEach ($u in $users) {
@@ -86,5 +119,3 @@ ForEach ($u in $users) {
     Write-Host "Creating $display in $path"
     New-ADUser -GivenName $u.First -Surname $u.Last -Name $display -DisplayName $display -SamAccountName $u.UserID -UserPrincipalName $UPN -Path $path -Department $u.Department
 }
-
-#endregion
