c217008ded8ab9744f426e781803ea88b4e40bdd
www42 <thomas.jaekel@gfn.training>
2021-10-06 12:24:58 +0200

    Update Lab8.ps1

--- 10961C Lab8.ps1
+++ 10961C Lab8.ps1
@@ -52,35 +52,59 @@ Set-ExecutionPolicy Unrestricted -Force
 
 # Exercise 2: Processing an Array with a ForeEach Loop
 # ----------------------------------------------------
-
+# 1. Open PowerShell
+# 2. Create Group Named IPPhoneTest
 New-ADGroup -Name IPPhoneTest -GroupScope Universal -GroupCategory Security
+
+# 3. Move IPPhoneTest to IT OU
 Move-ADObject "CN=IPPhoneTest,CN=Users,DC=Adatum,DC=com" -TargetPath "OU=IT,DC=Adatum,DC=com"
+
+# 4. Add Users to IPPhoneTest Group
 Add-ADGroupMember IPPhoneTest -Members Abbi, Ida, Parsa, Tonia
 
+# 5. Create ipPhone.ps1 File
+New-Item -ItemType File -Path E:\Mod08\Labfiles\ipPhone.ps1
+psEdit E:\Mod08\Labfiles\ipPhone.ps1
+
+# 6. Create Query for IPPhoneTest Membership
 $users = Get-ADGroupMember IPPhoneTest
 
+# 7. Create ForEach Loop to Process Users
 ForEach ($u in $users) {
     $fullUser = Get-ADUser $u
     $ipPhone = $fullUser.GivenName + "." + $fullUser.Surname + "@adatum.com"
     Set-ADUser $fullUser -replace @{ipPhone = "$ipPhone"}
 }
 
-Get-ADUser -Identity Abbi -Properties ipPhone | ft Name, ipPhone
-Get-ADUser -Identity Beth -Properties ipPhone | ft Name, ipPhone
+# 8. Run ipPhone.ps1
+E:\Mod08\Labfiles\ipPhone.ps1
+
 
 
 
 
 # Exercise 3: Processing Items by Using If Statements
 # ---------------------------------------------------
-
+# 1. Open PowerShell
+# 2. Set Work Location to E:\Mod08\Labfiles
 Set-Location E:\Mod08\Labfiles
-New-Item services.txt -ItemType File
-Get-Service "Print Spooler" | Select -ExpandProperty Name | Out-File .\services.txt -Append
-Get-Service "Windows Time" | Select -ExpandProperty Name | Out-File .\services.txt -Append
-cat .\services.txt
 
+# 3. Create File Named services.txt
+New-Item services.txt -ItemType File
+
+# 4. Find Print Spooler Name and Add it to services.txt
+Get-Service "Print Spooler" | Select -ExpandProperty Name | Out-File .\services.txt -Append
+
+# 5. Find Windows Time Name and Add it to services.txt
+Get-Service "Windows Time" | Select -ExpandProperty Name | Out-File .\services.txt -Append
+
+# 6. Create Script Named StartServices.ps1
+New-Item -ItemType file -Path E:\Mod08\Labfiles\StartServices.ps1
+
+# 7. Add Service Names to Variables from services.txt
 $Services = Get-Content .\services.txt
+
+# 8. Use Loop to Process Each Service
 foreach ($s in $Services) {
     $status = (Get-Service $s).Status
     if ($status -ne "Running") {
@@ -94,24 +118,40 @@ foreach ($s in $Services) {
 
 
 
+
+
 # Exercise 4: Creating a Random Password
 # --------------------------------------
+# 1. Create Script Named CreatePasswords.ps1
+New-Item -ItemType file -Path E:\Mod08\Labfiles\CreatePasswords.ps1
 
+# 2. Set $passwordLength Variable
 $passwordLength = 8
+
+# 3. Use For to Construct Loop
 $password = $null
 for ($i = 0; $i -le $passwordLength; $i++) {
     $number = Get-Random -Minimum 65 -Maximum 90
     $letter = [char]$number
     $password += $letter
 }
+
+# 4. Set the Password to Display on Screen
 Write-Output "The password is: $password"
+
+
 
 
 
 # Exercise 5: Creating Users Based on a CSV File
 # ----------------------------------------------
+# 1. Create Script Named CreateUsers.ps1
+New-Item -ItemType file -Path E:\Mod08\Labfiles\CreateUsers.ps1
 
+# 2. Set Script to Store users.csv Contents in Variable
 $users = Import-Csv -Path users.csv
+
+# 3. Create ForEachLoop to Create Users
 ForEach ($u in $users) {
     $path = "OU=" + $u.Department + ",DC=Adatum,DC=com"
     $upn = $u.UserID + "@adatum.com"
